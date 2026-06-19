ccc235494adf0c9fe4428dc49cef5f324cd0af47
Bruno2006K <bruluan480@gmail.com>
2026-06-18 20:07:56 -0500

    feat: configurar integración y rutas proxy hacia el alb

--- iac/computing/api_gateway.tf
+++ iac/computing/api_gateway.tf
@@ -10,3 +10,19 @@ resource "aws_apigatewayv2_vpc_link" "vpclink_apigw_to_alb" {
   security_group_ids = [var.vpc_link_sg_id]
   subnet_ids         = var.private_subnets
 }
+
+#Integracion con vpc link y ALB
+resource "aws_apigatewayv2_integration" "apigw_integration" {
+  api_id            = aws_apigatewayv2_api.apigw_http_endpoint.id
+  integration_type  = "HTTP_PROXY"
+  integration_uri   = aws_lb_listener.front_end.arn
+
+  integration_method        = "ANY"
+  connection_type           = "VPC_LINK"
+  connection_id             = aws_apigatewayv2_vpc_link.vpclink_apigw_to_alb.id
+  content_handling_strategy = "CONVERT_TO_TEXT"
+  payload_format_version    = "1.0"
+  depends_on                = [aws_apigatewayv2_vpc_link.vpclink_apigw_to_alb,
+                                aws_apigatewayv2_api.apigw_http_endpoint,
+                                aws_lb_listener.front_end]
+}
