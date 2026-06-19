262c1a2d54f1fc90545c03ff71a3b5e3746e074d
Bruno2006K <bruluan480@gmail.com>
2026-06-18 20:12:30 -0500

    feat: crear servicio fargate acoplado al target group del alb

---
+++ iac/computing/ecs.tf
@@ -0,0 +1,37 @@
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "${var.project_name}-ecs-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  tags = {
+    Name = "${var.project_name}-ecs-cluster"
+  }
+}
+
+resource "aws_ecs_service" "demo-ecs-service" {
+    name            = "demo-ecs-svc"
+    cluster         = aws_ecs_cluster.ecs_cluster.id
+    task_definition = aws_ecs_task_definition.ecs_taskdef.arn
+    desired_count   = 2
+    
+    deployment_maximum_percent         = 200
+    deployment_minimum_healthy_percent = 50
+    
+    launch_type = "FARGATE"
+    depends_on  = [aws_lb_target_group.alb_ecs_tg, aws_lb_listener.front_end]
+
+    load_balancer {
+        target_group_arn = aws_lb_target_group.alb_ecs_tg.arn
+        container_name   = "web"
+        container_port   = 8080
+    }
+
+    network_configuration {
+        security_groups  = [var.ecs_sg_id]
+        subnets          = var.private_subnets
+        assign_public_ip = false
+    }
+}
