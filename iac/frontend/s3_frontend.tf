f9ac9411005c02e791751964c27c4be62b7e9af1
JhonTMich <jhontmichael@gmail.com>
2026-06-18 19:00:54 -0500

    refactor: cambio de estructura a módulos IAM, variables y outputs

--- iac/frontend/s3_frontend.tf
+++ iac/frontend/s3_frontend.tf
@@ -1,46 +1,16 @@
 resource "aws_s3_bucket" "frontend" {
-  # Cambia esto si el nombre ya existe a nivel mundial en AWS
-  bucket = "golden-bears-frontend-app-bucket" 
+  bucket = var.bucket_name
 }
 
 resource "aws_s3_bucket_public_access_block" "frontend_public_block" {
   bucket = aws_s3_bucket.frontend.id
-
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_policy" "frontend_oac_policy" {
-  bucket = aws_s3_bucket.frontend.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "AllowCloudFrontServicePrincipal"
-        Effect    = "Allow"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-        Action   = "s3:GetObject"
-        Resource = "${aws_s3_bucket.frontend.arn}/*"
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = aws_cloudfront_distribution.frontend_cdn.arn
-          }
-        }
-      }
-    ]
-  })
-}
-
-# solucion a error CKV_AWS_21
 resource "aws_s3_bucket_versioning" "frontend_versioning" {
   bucket = aws_s3_bucket.frontend.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
+  versioning_configuration { status = "Enabled" }
 }
