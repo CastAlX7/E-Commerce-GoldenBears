cf6ee6fe127669bab6e5556ee74975d59bdd4e38
Bruno2006K <bruluan480@gmail.com>
2026-06-18 20:21:26 -0500

    feat: crear rol de tarea para los permisos de la aplicación en ecs

--- iac/computing/iam.tf
+++ iac/computing/iam.tf
@@ -25,3 +25,25 @@ resource "aws_iam_role_policy_attachment" "ecs_task_exec_role_policy" {
   role       = aws_iam_role.ecs_task_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+# IAM Role para la aplicación dentro del contenedor
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.project_name}-ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-ecs-task-role"
+  }
+}
