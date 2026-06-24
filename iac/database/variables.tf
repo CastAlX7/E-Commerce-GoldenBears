1d51b110fc7f310458983af9775ba80ca167f15c
Bruno2006K <bruluan480@gmail.com>
2026-06-24 00:25:46 -0500

    feat: añadir variable environment para soporte multi-entorno

--- iac/database/variables.tf
+++ iac/database/variables.tf
@@ -2,3 +2,8 @@ variable "project_name" {
   type        = string
   description = "Nombre del proyecto, usado para el etiquetado y nombres de los recursos"
 }
+
+variable "environment" {
+  type        = string
+  description = "Entorno de despliegue (dev, stg, prod)"
+}
