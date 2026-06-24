0c149c24222019a48595b01a40242ed0a858f021
Bruno2006K <bruluan480@gmail.com>
2026-06-24 00:26:36 -0500

    feat: añadir variable private_db_subnet_ids

--- iac/database/variables.tf
+++ iac/database/variables.tf
@@ -7,3 +7,8 @@ variable "environment" {
   type        = string
   description = "Entorno de despliegue (dev, stg, prod)"
 }
+
+variable "private_db_subnet_ids" {
+  type        = list(string)
+  description = "Lista de IDs de las subredes aisladas destinadas para el clúster de Aurora"
+}
