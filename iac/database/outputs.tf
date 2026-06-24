abdedd9b475aaef905f78ae40aa1f20052eb630a
Bruno2006K <bruluan480@gmail.com>
2026-06-24 00:16:59 -0500

    feat: añadir salida de endpoint directo de escritura para Aurora

--- iac/database/outputs.tf
+++ iac/database/outputs.tf
@@ -12,3 +12,8 @@ output "rds_proxy_resource_id" {
   value       = aws_db_proxy.aurora_proxy.id
   description = "El ID de recurso del RDS Proxy. Lo requiere el lambda de inventario"
 }
+
+output "aurora_cluster_endpoint" {
+  value       = aws_rds_cluster.aurora.endpoint
+  description = "Endpoint directo del clúster de Aurora (Escritura) solo para migraciones de datos o scripts de emergencia."
+}
