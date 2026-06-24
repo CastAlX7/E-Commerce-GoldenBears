437cf56f2875b2a3854c6fb81a2b647ae32d8987
Bruno2006K <bruluan480@gmail.com>
2026-06-24 00:15:38 -0500

    feat: añadir salida de ID de recurso para RDS Proxy

--- iac/database/outputs.tf
+++ iac/database/outputs.tf
@@ -7,3 +7,8 @@ output "rds_proxy_arn" {
   value       = aws_db_proxy.aurora_proxy.arn
   description = "ARN del RDS Proxy."
 }
+
+output "rds_proxy_resource_id" {
+  value       = aws_db_proxy.aurora_proxy.id
+  description = "El ID de recurso del RDS Proxy. Lo requiere el lambda de inventario"
+}
