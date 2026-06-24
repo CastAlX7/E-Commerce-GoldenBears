83a52e6768324eca22d13835d871634cc77607d0
CastAlX7 <stilesvcc@gmail.com>
2026-06-24 14:51:07 -0500

    fix: eliminar defaults hardcodeados de las variables

---
+++ iac/presentacion/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  type        = string
+  description = "Nombre del bucket S3 para el frontend"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "Dominio principal del proyecto"
+}
