d97b09efb4f1dda2fcb45db1ba61997cbce3e33a
CastAlX7 <stilesvcc@gmail.com>
2026-06-24 17:12:37 -0500

    refactor: eliminar carpeta frontend renombrada

--- iac/frontend/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  type        = string
-  default     = "golden-bears-frontend-app-bucket"
-}
-
-variable "domain_name" {
-  type        = string
-  default     = "goldenbears.com" #aun para ver
-}
