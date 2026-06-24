1683c751228a7aaec6273d74f277b2d2d26ffd2d
Bruno2006K <bruluan480@gmail.com>
2026-06-24 00:14:21 -0500

    feat: añadir salida de ARN para RDS Proxy

--- iac/database/outputs.tf
+++ iac/database/outputs.tf
@@ -2,3 +2,8 @@ output "rds_proxy_endpoint" {
   value       = aws_db_proxy.aurora_proxy.endpoint
   description = "Endpoint del RDS Proxy"
 }
+
+output "rds_proxy_arn" {
+  value       = aws_db_proxy.aurora_proxy.arn
+  description = "ARN del RDS Proxy."
+}
