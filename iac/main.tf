7e7dc363548298c5d393a4b08577b06c7e42caa0
CastAlX7 <stilesvcc@gmail.com>
2026-06-24 15:01:12 -0500

    feat: conectar todos los módulos en main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -7,3 +7,50 @@ module "networking" {
   aws_region   = var.region
 }
 
+module "security" {
+  source           = "./security"
+  project_name     = var.project_name
+  redis_auth_token = var.redis_auth_token
+}
+
+module "database" {
+  source                 = "./database"
+  project_name           = var.project_name
+  environment            = var.environment
+  private_db_subnet_ids  = module.networking.private_db_subnet_ids
+  private_app_subnet_ids = module.networking.private_app_subnet_ids
+  aurora_sg_id           = module.networking.aurora_sg_id
+  rds_proxy_sg_id        = module.networking.rds_proxy_sg_id
+  elasticache_sg_id      = module.networking.elasticache_sg_id
+  redis_auth_token       = var.redis_auth_token
+}
+
+module "messaging" {
+  source                   = "./messaging"
+  project_name             = var.project_name
+  environment              = var.environment
+  region                   = var.region
+  private_lambda_subnet_id = module.networking.private_lambda_subnet_id
+  lambda_inv_sg_id         = module.networking.lambda_inventario_sg_id
+  rds_proxy_resource_id    = module.database.rds_proxy_resource_id
+  nubefact_secret_arn      = module.security.nubefact_secret_arn
+}
+
+module "computing" {
+  source              = "./computing"
+  project_name        = var.project_name
+  environment         = var.environment
+  vpc_id              = module.networking.vpc_id
+  private_subnets     = module.networking.private_app_subnet_ids
+  ecs_sg_id           = module.networking.ecs_sg_id
+  alb_sg_id           = module.networking.alb_sg_id
+  vpc_link_sg_id      = module.networking.vpc_link_sg_id
+  app_db_secret_arn   = module.security.app_db_secret_arn
+  secrets_kms_key_arn = module.security.secrets_kms_key_arn
+}
+
+module "presentacion" {
+  source      = "./presentacion"
+  bucket_name = "${var.project_name}-frontend-app-bucket"
+  domain_name = var.domain_name
+}
