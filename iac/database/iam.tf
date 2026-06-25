42e683bd031f93b79fcc2f4b9ffd9d36c100c5a5
Bruno2006K <bruluan480@gmail.com>
2026-06-25 01:05:44 -0500

    fix: habilitar enhanced monitoring en instancias RDS para cumplir con CKV_AWS_118

--- iac/database/iam.tf
+++ iac/database/iam.tf
@@ -35,3 +35,21 @@ resource "aws_iam_role_policy_attachment" "rds_proxy_attach" {
   role       = aws_iam_role.rds_proxy_role.name
   policy_arn = aws_iam_policy.rds_proxy_policy.arn
 }
+
+resource "aws_iam_role" "rds_enhanced_monitoring" {
+  name = "${var.project_name}-rds-monitoring-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "monitoring.rds.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring" {
+  role       = aws_iam_role.rds_enhanced_monitoring.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
