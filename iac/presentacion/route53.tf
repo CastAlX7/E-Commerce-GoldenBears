3bd4c8d2c11f96c3677a65aff5bd04d80b66c40c
JhonTMich <jhontmichael@gmail.com>
2026-06-25 00:33:22 -0500

    fix: firmas criptograficas DNSSEC en Route 53, correccion error CKV2_AWS_39

--- iac/presentacion/route53.tf
+++ iac/presentacion/route53.tf
@@ -8,6 +8,35 @@ resource "aws_route53_query_log" "main_query_log" {
   zone_id                  = aws_route53_zone.main.zone_id
 }
 
+resource "aws_kms_key" "dnssec_key" {
+  description              = "KMS para Route53 DNSSEC"
+  customer_master_key_spec = "ECC_NIST_P256"
+  key_usage                = "SIGN_VERIFY"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = { Service = "route53.amazonaws.com" }
+        Action = ["kms:DescribeKey", "kms:GetPublicKey", "kms:Sign"]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_route53_key_signing_key" "main_ksk" {
+  hosted_zone_id             = aws_route53_zone.main.zone_id
+  key_management_service_arn = aws_kms_key.dnssec_key.arn
+  name                       = "goldenbears-ksk"
+  status                     = "ACTIVE"
+}
+
+resource "aws_route53_hosted_zone_dnssec" "main_dnssec" {
+  depends_on     = [aws_route53_key_signing_key.main_ksk]
+  hosted_zone_id = aws_route53_key_signing_key.main_ksk.hosted_zone_id
+}
+
 resource "aws_route53_record" "frontend_dns" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "www.goldenbears.com"
