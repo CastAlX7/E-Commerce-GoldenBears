731405eb058c0b608c644d9dde7fa6764aa98da2
Bruno2006K <bruluan480@gmail.com>
2026-06-24 23:07:03 -0500

    feat: permitir que la cuenta de servicio de alb escriba logs en s3

--- iac/messaging/s3_comprobaciones.tf
+++ iac/messaging/s3_comprobaciones.tf
@@ -62,3 +62,21 @@ resource "aws_s3_bucket_lifecycle_configuration" "lifecycle_comprobantes" {
     # Los objetos NO se eliminan automáticamente.
   }
 }
+
+data "aws_elb_service_account" "main" {}
+
+resource "aws_s3_bucket_policy" "alb_logs_policy" {
+  bucket = aws_s3_bucket.documental.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { AWS = data.aws_elb_service_account.main.arn }
+        Action    = "s3:PutObject"
+        Resource  = "${aws_s3_bucket.documental.arn}/alb/*"
+      }
+    ]
+  })
+}
