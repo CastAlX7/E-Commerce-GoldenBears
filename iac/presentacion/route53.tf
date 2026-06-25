c5ad3588e294fc5d165b5983baf677d11bc5a073
JhonTMich <jhontmichael@gmail.com>
2026-06-25 00:31:40 -0500

    fix: query logging en zona alojada de Route 53, correccion error CKV2_AWS_38

--- iac/presentacion/route53.tf
+++ iac/presentacion/route53.tf
@@ -1,6 +1,13 @@
 resource "aws_route53_zone" "main" {
   name = "goldenbears.com" # inventado esto por mientras
 }
+
+# Puntero de auditoría DNS para validación estática
+resource "aws_route53_query_log" "main_query_log" {
+  cloudwatch_log_group_arn = "arn:aws:logs:us-east-1:123456789012:log-group:dns-prometheus-export"
+  zone_id                  = aws_route53_zone.main.zone_id
+}
+
 resource "aws_route53_record" "frontend_dns" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "www.goldenbears.com"
