81e6297fdeacf823867dbc3818925175c4365950
JhonTMich <jhontmichael@gmail.com>
2026-06-24 21:34:09 -0500

    refactor: renombrar modulo frontend a presentacion

---
+++ iac/presentacion/iam.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket_policy" "frontend_oac_policy" {
+  bucket = aws_s3_bucket.frontend.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "AllowCloudFrontServicePrincipal"
+      Effect    = "Allow"
+      Principal = { Service = "cloudfront.amazonaws.com" }
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      Condition = {
+        StringEquals = { "AWS:SourceArn" = aws_cloudfront_distribution.frontend_cdn.arn }
+      }
+    }]
+  })
+}
---
+++ iac/presentacion/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  type        = string
+  default     = "golden-bears-frontend-app-bucket"
+}
+
+variable "domain_name" {
+  type        = string
+  default     = "goldenbears.com" #aun para ver
+}
