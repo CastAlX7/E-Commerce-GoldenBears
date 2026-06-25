032099fe01109e200d25d6bdbfa15c72f4018ade
Bruno2006K <bruluan480@gmail.com>
2026-06-24 23:02:49 -0500

    feat: se agrega output documental para logs y comprobantes

--- iac/computing/outputs.tf
+++ iac/computing/outputs.tf
@@ -12,3 +12,8 @@ output "alb_dns_name" {
   value       = aws_lb.ecs_alb.dns_name
   description = "DNS del ALB"
 }
+
+output "documental_bucket_name" {
+  value       = aws_s3_bucket.documental.bucket
+  description = "Nombre del bucket S3 documental para logs y comprobantes"
+}
