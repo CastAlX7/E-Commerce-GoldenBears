7a126f842440a4c41a16db52a9495dd91289778d
JhonTMich <jhontmichael@gmail.com>
2026-06-24 23:29:25 -0500

    fix: cabeceras de seguridad HTTP en distribucion CDN, correccion error CKV2_AWS_32

--- iac/presentacion/cloudfront.tf
+++ iac/presentacion/cloudfront.tf
@@ -10,6 +10,36 @@ resource "aws_cloudfront_origin_access_control" "frontend_oac" {
   signing_protocol                  = "sigv4"
 }
 
+# Política de cabeceras de seguridad HTTP
+resource "aws_cloudfront_response_headers_policy" "security_headers" {
+  name = "golden-bears-security-headers-policy"
+
+  security_headers_config {
+    strict_transport_security {
+      access_control_max_age_sec = 31536000
+      include_subdomains         = true  
+      preload                    = true
+      override                   = true
+    }
+    frame_options {
+      frame_option = "DENY"
+      override     = true
+    }
+    content_type_options {
+      override = true
+    }
+    xss_protection {
+      mode_block = true
+      protection = true
+      override   = true
+    }
+    referrer_policy {
+      referrer_policy = "same-origin"
+      override        = true
+    }
+  }
+}
+
 resource "aws_cloudfront_distribution" "frontend_cdn" {
   origin {
     domain_name              = aws_s3_bucket.frontend.bucket_regional_domain_name
@@ -35,10 +65,11 @@ resource "aws_cloudfront_distribution" "frontend_cdn" {
       }
     }
 
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    viewer_protocol_policy     = "redirect-to-https"
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.security_headers.id
+    min_ttl                    = 0
+    default_ttl                = 3600
+    max_ttl                    = 86400
   }
 
   restrictions {
