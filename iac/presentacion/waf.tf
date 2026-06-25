864258156dc30008cba3faeee490a33edaa18a97
JhonTMich <jhontmichael@gmail.com>
2026-06-25 00:14:50 -0500

    fix: regla KnownBadInputs en WAF para mitigar vulnerabilidades Log4j, correccion error CKV_AWS_192

--- iac/presentacion/waf.tf
+++ iac/presentacion/waf.tf
@@ -28,6 +28,29 @@ resource "aws_wafv2_web_acl" "frontend_waf" {
     }
   }
 
+  # Grupo de reglas para mitigar Log4j y Bad Inputs
+  rule {
+    name     = "AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesLog4jMetric"
+      sampled_requests_enabled   = true
+    }
+  }
+
   visibility_config {
     cloudwatch_metrics_enabled = true
     metric_name                = "GoldenBearsFrontendWafMetric"
