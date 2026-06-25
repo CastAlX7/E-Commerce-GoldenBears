9a1fbc836d893687ef2a0cc7b3eefa9e43436df3
Bruno2006K <bruluan480@gmail.com>
2026-06-25 13:59:38 -0500

    feat: solucion de outputs.tf

--- iac/computing/outputs.tf
+++ iac/computing/outputs.tf
@@ -12,18 +12,3 @@ output "alb_dns_name" {
   value       = aws_lb.ecs_alb.dns_name
   description = "DNS del ALB"
 }
-
-output "documental_bucket_name" {
-  value       = aws_s3_bucket.documental.bucket
-  description = "Nombre del bucket S3 documental para logs y comprobantes"
-}
-
-output "sns_orders_topic_arn" {
-  value       = aws_sns_topic.orders_topic.arn
-  description = "ARN del topic SNS de ordenes"
-}
-
-output "sqs_inventory_queue_arn" {
-  value       = aws_sqs_queue.inventory_queue.arn
-  description = "ARN de la cola SQS de inventario"
-}
