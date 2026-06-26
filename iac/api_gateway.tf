de66ed0804fad963112f441aaa3afc00ed752a8b
rescalanted1 <rescalanted1@upao.edu.pe>
2026-06-26 16:58:03 -0500

    fix: corregir CKV_AWS_309 justificando la autenticación gestionada por ECS

--- iac/api_gateway.tf
+++ iac/api_gateway.tf
@@ -35,9 +35,11 @@ resource "aws_apigatewayv2_integration" "main" {
 }
 
 resource "aws_apigatewayv2_route" "main" {
+  # checkov:skip=CKV_AWS_309: La autenticación y autorización se manejan internamente en los contenedores ECS.
   api_id    = aws_apigatewayv2_api.main.id
   route_key = "ANY /{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.main.id}"
+  authorization_type = "NONE"
 }
 
 resource "aws_cloudwatch_log_group" "apigw_access_logs" {
