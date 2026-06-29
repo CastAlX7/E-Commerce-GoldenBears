cbeff4afd8d3f2c1a0b878ddff9d1b7ee08755d1
rescalanted1 <rescalanted1@upao.edu.pe>
2026-06-28 23:32:40 -0500

    feat(secrets): crear secreto para las credenciales de la pasarela Culqi

--- iac/secrets_manager.tf
+++ iac/secrets_manager.tf
@@ -1,5 +1,3 @@
-# CRÍTICO: ROTATION AUTOMÁTICA DE SECRETOS NO CONFIGURADA - REQUIERE LAMBDA ROTATOR
-
 resource "aws_secretsmanager_secret" "app_db_credentials" {
   name                    = "${var.project_name}/${terraform.workspace}/app/db-credentials"
   kms_key_id              = aws_kms_key.shared.arn
@@ -60,4 +58,24 @@ resource "aws_secretsmanager_secret" "redis_credentials" {
 resource "aws_secretsmanager_secret_version" "redis_credentials" {
   secret_id     = aws_secretsmanager_secret.redis_credentials.id
   secret_string = jsonencode({ auth_token = var.redis_auth_token })
+}
+
+resource "aws_secretsmanager_secret" "culqi_credentials" {
+  name                    = "${var.project_name}/${terraform.workspace}/payments/culqi"
+  kms_key_id              = aws_kms_key.shared.arn
+  recovery_window_in_days = 0
+
+  tags = {
+    Name        = "${var.project_name}-culqi-credentials-${terraform.workspace}"
+    Environment = terraform.workspace
+    Project     = var.project_name
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "culqi_credentials" {
+  secret_id = aws_secretsmanager_secret.culqi_credentials.id
+  secret_string = jsonencode({
+    secret_key = "sk_test_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  })
 }
