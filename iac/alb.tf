2d90b98376b2e9a40706ec8387a6a44f3526f773
CastAlX7 <stilesvcc@gmail.com>
2026-06-29 00:19:03 -0500

    chore: omitir regla CKV_AWS_378 para protocolo HTTP en ALB interno

--- iac/alb.tf
+++ iac/alb.tf
@@ -26,6 +26,8 @@ resource "aws_lb" "ecs_alb" {
 }
 
 resource "aws_lb_target_group" "main" {
+
+  # checkov:skip=CKV_AWS_378: El ALB es interno y se accede de forma segura via VPC Link desde API Gateway. El trafico interno fluye sobre HTTP en el puerto 8000 hacia los contenedores ECS Fargate para evitar la sobrecarga de gestionar TLS en el backend.
   name        = "${var.project_name}-tg-${terraform.workspace}"
   port        = 8000
   protocol    = "HTTP"
@@ -50,6 +52,7 @@ resource "aws_lb_target_group" "main" {
 }
 
 resource "aws_lb_listener" "main" {
+    # checkov:skip=CKV_AWS_378: El ALB es interno y se accede de forma segura via VPC Link desde API Gateway. El puerto 8000 usa HTTP en la red privada de la VPC.
   load_balancer_arn = aws_lb.ecs_alb.arn
   port              = 8000
   protocol          = "HTTP"
