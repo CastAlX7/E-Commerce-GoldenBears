2acbf22346556a9ee2a68e22bf9cff3c5ff4995d
CastAlX7 <stilesvcc@gmail.com>
2026-07-01 03:40:08 -0500

    refactor: deshabilitar zona Route53 al usar dominio default de CloudFront

--- iac/route53.tf
+++ iac/route53.tf
@@ -1,77 +1,79 @@
-resource "aws_route53_zone" "main" {
-  name = var.domain_name
+# La zona DNS de Route 53 y sus registros alias no son necesarios al desplegar con el dominio por defecto de CloudFront.
+# resource "aws_route53_zone" "main" {
+#   name = var.domain_name
+# 
+#   tags = {
+#     Name        = "${var.project_name}-zone-${terraform.workspace}"
+#     Environment = terraform.workspace
+#     Project     = var.project_name
+#     ManagedBy   = "Terraform"
+#   }
+# }
+# 
+# resource "aws_cloudwatch_log_group" "route53_query_logs" {
+#   provider          = aws.us_east_1
+#   name              = "/aws/route53/${var.domain_name}"
+#   retention_in_days = var.log_retention_days
+#   kms_key_id        = aws_kms_key.shared.arn
+# 
+#   tags = {
+#     Name        = "${var.project_name}-route53-logs-${terraform.workspace}"
+#     Environment = terraform.workspace
+#     Project     = var.project_name
+#     ManagedBy   = "Terraform"
+#   }
+# }
+# 
+# resource "aws_cloudwatch_log_resource_policy" "route53_query_logs" {
+#   provider    = aws.us_east_1
+#   policy_name = "${var.project_name}-route53-logs-policy-${terraform.workspace}"
+# 
+#   policy_document = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Effect = "Allow"
+#       Principal = {
+#         Service = "route53.amazonaws.com"
+#       }
+#       Action = [
+#         "logs:CreateLogStream",
+#         "logs:PutLogEvents"
+#       ]
+#       Resource = "${aws_cloudwatch_log_group.route53_query_logs.arn}:*"
+#     }]
+#   })
+# }
+# 
+# resource "aws_route53_query_log" "main" {
+#   cloudwatch_log_group_arn = aws_cloudwatch_log_group.route53_query_logs.arn
+#   zone_id                  = aws_route53_zone.main.zone_id
+# 
+#   depends_on = [aws_cloudwatch_log_resource_policy.route53_query_logs]
+# }
+# 
+# 
+# resource "aws_route53_key_signing_key" "main" {
+#   hosted_zone_id             = aws_route53_zone.main.zone_id
+#   key_management_service_arn = aws_kms_key.dnssec.arn
+#   name                       = "${var.project_name}-ksk-${terraform.workspace}"
+#   status                     = "ACTIVE"
+# }
+# 
+# resource "aws_route53_hosted_zone_dnssec" "main" {
+#   hosted_zone_id = aws_route53_key_signing_key.main.hosted_zone_id
+# 
+#   depends_on = [aws_route53_key_signing_key.main]
+# }
+# 
+# resource "aws_route53_record" "www" {
+#   zone_id = aws_route53_zone.main.zone_id
+#   name    = "www.${var.domain_name}"
+#   type    = "A"
+# 
+#   alias {
+#     name                   = aws_cloudfront_distribution.frontend_cdn.domain_name
+#     zone_id                = aws_cloudfront_distribution.frontend_cdn.hosted_zone_id
+#     evaluate_target_health = false
+#   }
+# }
 
-  tags = {
-    Name        = "${var.project_name}-zone-${terraform.workspace}"
-    Environment = terraform.workspace
-    Project     = var.project_name
-    ManagedBy   = "Terraform"
-  }
-}
-
-resource "aws_cloudwatch_log_group" "route53_query_logs" {
-  provider          = aws.us_east_1
-  name              = "/aws/route53/${var.domain_name}"
-  retention_in_days = var.log_retention_days
-  kms_key_id        = aws_kms_key.shared.arn
-
-  tags = {
-    Name        = "${var.project_name}-route53-logs-${terraform.workspace}"
-    Environment = terraform.workspace
-    Project     = var.project_name
-    ManagedBy   = "Terraform"
-  }
-}
-
-resource "aws_cloudwatch_log_resource_policy" "route53_query_logs" {
-  provider    = aws.us_east_1
-  policy_name = "${var.project_name}-route53-logs-policy-${terraform.workspace}"
-
-  policy_document = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "route53.amazonaws.com"
-      }
-      Action = [
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ]
-      Resource = "${aws_cloudwatch_log_group.route53_query_logs.arn}:*"
-    }]
-  })
-}
-
-resource "aws_route53_query_log" "main" {
-  cloudwatch_log_group_arn = aws_cloudwatch_log_group.route53_query_logs.arn
-  zone_id                  = aws_route53_zone.main.zone_id
-
-  depends_on = [aws_cloudwatch_log_resource_policy.route53_query_logs]
-}
-
-
-resource "aws_route53_key_signing_key" "main" {
-  hosted_zone_id             = aws_route53_zone.main.zone_id
-  key_management_service_arn = aws_kms_key.dnssec.arn
-  name                       = "${var.project_name}-ksk-${terraform.workspace}"
-  status                     = "ACTIVE"
-}
-
-resource "aws_route53_hosted_zone_dnssec" "main" {
-  hosted_zone_id = aws_route53_key_signing_key.main.hosted_zone_id
-
-  depends_on = [aws_route53_key_signing_key.main]
-}
-
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "www.${var.domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.frontend_cdn.domain_name
-    zone_id                = aws_cloudfront_distribution.frontend_cdn.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
