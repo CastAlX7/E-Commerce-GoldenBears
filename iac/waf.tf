97281b9384de5491f4dc710fa542823ce8222782
CastAlX7 <stilesvcc@gmail.com>
2026-07-01 03:43:00 -0500

    fix: corregir tilde en descripcion del WAF

--- iac/waf.tf
+++ iac/waf.tf
@@ -2,7 +2,7 @@ resource "aws_wafv2_web_acl" "frontend" {
   provider    = aws.us_east_1
   name        = "${var.project_name}-frontend-waf-${terraform.workspace}"
   scope       = "CLOUDFRONT"
-  description = "WAF para la distribución CloudFront del marketplace Golden Bears"
+  description = "WAF para la distribucion CloudFront del marketplace Golden Bears"
 
   default_action {
     allow {}
