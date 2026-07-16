8a8f9a37f490c768badfb9a16de1529b003acf47
CastAlX7 <stilesvcc@gmail.com>
2026-07-16 11:17:22 -0500

    fix: cambiar mutabilidad de tags a MUTABLE para permitir sobrescritura

--- iac/ecr.tf
+++ iac/ecr.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "backend" {
   name                 = "${var.project_name}-backend-${terraform.workspace}"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   force_delete         = true
 
   encryption_configuration {
@@ -21,7 +21,7 @@ resource "aws_ecr_repository" "backend" {
 
 resource "aws_ecr_repository" "prometheus" {
   name                 = "${var.project_name}-prometheus-${terraform.workspace}"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   force_delete         = true
 
   encryption_configuration {
@@ -45,7 +45,7 @@ resource "aws_ecr_repository" "prometheus" {
 # provisionado automáticamente al arrancar el contenedor.
 resource "aws_ecr_repository" "grafana" {
   name                 = "${var.project_name}-grafana-${terraform.workspace}"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   force_delete         = true
 
   encryption_configuration {
